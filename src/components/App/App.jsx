26c99ea976aeec467c855e3cb73ea36c881ba6ea
Nadiia Tsytsylina <miniova95@gmail.com>
2023-06-07 19:11:11 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,13 +13,13 @@ export default function App() {
   const onClick = option => {
     switch (option) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
 
       default:
